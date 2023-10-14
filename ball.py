f64db9325dfe85d350a24fb8514192be187df3a0
bheelz <abioladeyeye@gmail.com>
2023-10-14 19:02:36 +0100

    Added ball bounce on all sides

--- ball.py
+++ ball.py
@@ -17,19 +17,13 @@ class Ball:
     def move_ball(self):
         self.ball.left(30)
         while True:
-            self.ball.speed("slow")
+            self.ball.speed(0)
             self.ball.forward(30)
             self.x_coordinate = self.ball.xcor()
             self.y_coordinate = self.ball.ycor()
-            if self.x_coordinate > 380:
-                self.ball.left(60)
-                self.ball.forward(30)
-            elif self.x_coordinate < -380:
-                self.ball.left(60)
-                self.ball.forward(30)
-            elif self.y_coordinate > 380:
-                self.ball.left(60)
-                self.ball.forward(30)
-            elif self.y_coordinate < -370:
-                self.ball.left(60)
-                self.ball.forward(30)
+            if self.x_coordinate > 390 or self.x_coordinate < -390:
+                # Change horizontal direction on hitting the edges
+                self.ball.setheading(180 - self.ball.heading())
+            if self.y_coordinate > 290 or self.y_coordinate < -280:
+                # Change vertical direction on hitting the edges
+                self.ball.setheading(-self.ball.heading())
